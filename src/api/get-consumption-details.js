75983f5bac9a37cc06888690f0049c39e1f459b9
Joy Kim <30822283+feel5ny@users.noreply.github.com>
2023-03-13 01:18:11 +0900

    getConsumptionDetails 연동

---
+++ src/api/get-consumption-details.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+/**
+ * @returns
+ * {
+ *  id: number,
+ *  price: number,
+ *  category: string,
+ *  fundsAtTheTime: number,
+ *  description: string,
+ *  createAt: string,
+ * }[]
+ */
+export const getConsumptionDetails = async () => {
+  const { data } = await axios.get("http://localhost:3002/consumption-details");
+  return data;
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,11 @@
 import "@fortawesome/fontawesome-free/js/all.min.js";
 import "../style/index.scss";
+import { initConsumptionDetails } from "./consumption-details";
 import { initCurrentAsset } from "./current-asset";
 
 const initApp = () => {
   initCurrentAsset();
-  // initXX();
+  initConsumptionDetails();
   // initXX();
 };
 
